02bb28861cc18fe2e735ce38fd3db9fd2d96c377
Emre Cüni <cuniiemre@gmail.com>
2024-12-22 15:57:49 +0300

    insert registry with ef

---
+++ EntityFramework/Controllers/StudentController.cs
@@ -0,0 +1,28 @@
+﻿using EntityFramework.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFramework.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly DataContext _dataContext;
+
+        public StudentController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Student model)
+        {
+            _dataContext.Students.Add(model);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction("Index","Home");
+        }
+    }
+}
