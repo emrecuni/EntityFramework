e2561ab24e2f0da65b5baf233fdc88628dfc9f74
Emre Cüni <cuniiemre@gmail.com>
2024-12-28 16:06:10 +0300

    course added

---
+++ EntityFramework/Controllers/CourseController.cs
@@ -0,0 +1,28 @@
+﻿using EntityFramework.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EntityFramework.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly DataContext _dataContext;
+        public CourseController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Course model)
+        {
+            _dataContext.Courses.Add(model);
+            await _dataContext.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
